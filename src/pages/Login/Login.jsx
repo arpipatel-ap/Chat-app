3a1a84171e23de4d563c4cdb7069869e4c840c48
arpipatel-ap <arpipatel96@gmail.com>
2024-08-18 21:24:47 -0400

    add state for login and signup

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,14 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Login.css';
 import assets from '../../assets/assets';
 
 const Login = () => {
+
+  const [currState, setCurrState] = useState("Sign up");
+
   return (
     <div className='login'>
       <img src={assets.logo_big}alt="" className="logo" />
       <form className="login-form">
-        <h2>Sign Up</h2>
-        <input type="text" placeholder='username' className="form-input" required/>
+        <h2>{currState}</h2>
+        {currState === "Sign up"?<input type="text" placeholder='username' className="form-input" required /> :null}
         <input type="email" placeholder ='email address' className="form-input" required />
         <input type="password" placeholder='password' className="form-input" required/>
         <button type="submit">Sign Up</button>
@@ -17,7 +20,7 @@ const Login = () => {
           <p>Agree to the terms of use & privacy policy</p>
         </div>
         <div className="login-forgot">
-          <p className="login-toggle">Already have an account <span>click here</span> </p>
+          <p className="login-toggle">Already have an account <span onClick={()=>setCurrState("Login")}>click here</span> </p>
         </div>
       </form>
     </div>
