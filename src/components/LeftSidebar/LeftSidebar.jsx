f8967847fc5703f6e7424db08771245197567df6
arpipatel-ap <arpipatel96@gmail.com>
2024-08-19 20:47:48 -0400

    add jsx file for leftsidebar

--- src/components/LeftSidebar/LeftSidebar.jsx
+++ src/components/LeftSidebar/LeftSidebar.jsx
@@ -1,10 +1,33 @@
 import React from 'react';
-import "./LeftSidebar.cxss"
+import "./LeftSidebar.css";
+import assets from '../../assets/assets'
 
 const LeftSidebar = () => {
   return (
-    <div>LeftSidebar</div>
+    <div className='ls'>
+      <div className="ls-top">
+        <div className="ls-nav">
+          <img src={assets.logo} className ='logo' alt="" />
+          <div className="menu">
+            <img src={assets.menu_icon} alt="" />
+          </div>
+        </div>
+        <div className="ls-search">
+          <img src={assets.search_icon} alt="" />
+          <input type="text" placeholder='Search here..' />
+        </div>
+      </div>
+      <div className="ls-list">
+        <div className="friends">
+          <img src={assets.profile_img} alt="" />
+          <div>
+            <p>Richand Sanford</p>
+            <span>Hello, How are you?</span>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
-export default LeftSidebar
+export default LeftSidebar;
