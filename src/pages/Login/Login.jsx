0922b7b1e1327a612ee7d0f9741015336d2c439f
arpipatel-ap <arpipatel96@gmail.com>
2024-08-19 20:21:56 -0400

    make state for moving sign up to login

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -14,13 +14,18 @@ const Login = () => {
         {currState === "Sign up"?<input type="text" placeholder='username' className="form-input" required /> :null}
         <input type="email" placeholder ='email address' className="form-input" required />
         <input type="password" placeholder='password' className="form-input" required/>
-        <button type="submit">Sign Up</button>
+        <button type="submit">{currState === "Sign up"? "Create account": "Login now"}</button>
         <div className="login-term">
           <input type="checkbox" />
           <p>Agree to the terms of use & privacy policy</p>
         </div>
         <div className="login-forgot">
-          <p className="login-toggle">Already have an account <span onClick={()=>setCurrState("Login")}>click here</span> </p>
+          {
+            currState === "Sign up"
+            ?
+            <p className="login-toggle">Already have an account <span onClick={()=>setCurrState("Login")}>Login here</span> </p>
+            :<p className="login-toggle">Create an account<span onClick={()=>setCurrState("Sign up")}>click here</span> </p>
+          }
         </div>
       </form>
     </div>
