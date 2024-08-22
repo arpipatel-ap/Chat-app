de6c040c19a4f5a2911266b436a342969c903c28
arpipatel-ap <arpipatel96@gmail.com>
2024-08-21 21:01:12 -0400

    add properties of chat-box

--- src/components/ChatBox/ChatBox.jsx
+++ src/components/ChatBox/ChatBox.jsx
@@ -1,9 +1,16 @@
 import React from 'react';
 import "./ChatBox.css";
+import assets from '../../assets/assets';
 
 const ChatBox = () => {
   return (
-    <div>ChatBox</div>
+    <div className='chat-box'>
+      <div className="chat-user">
+        <img src={assets.profile_img} alt="" />
+        <p>Richard Sanford <img src={assets.green_dot} alt="" /></p>
+        <img src={assets.help_icon} className='help' alt="" />
+      </div>
+    </div>
   )
 }
 
