97f33ffd16e88872fd5ba3e4c54d3e20eb2764ca
arpipatel-ap <arpipatel96@gmail.com>
2024-08-22 21:15:40 -0400

    add properties for sending and receiving

--- src/components/ChatBox/ChatBox.jsx
+++ src/components/ChatBox/ChatBox.jsx
@@ -10,6 +10,26 @@ const ChatBox = () => {
         <p>Richard Sanford <img className='dot' src={assets.green_dot} alt="" /></p>
         <img src={assets.help_icon} className='help' alt="" />
       </div>
+
+    <div className="chat-msg">
+      <div className="s-msg">
+        <p className="msg">Lorem ipsum dolor sit amet...</p>
+        <div>
+          <img src={assets.profile_img} alt="" />
+          <p>2:30 p.m.</p>
+        </div>
+      </div>
+      <div className="r-msg">
+        <p className="msg">Lorem ipsum dolor sit amet...</p>
+        <div>
+          <img src={assets.profile_img} alt="" />
+          <p>2:30 p.m.</p>
+        </div>
+      </div>
+    </div>
+
+
+
       <div className="chat-input">
         <input type="text" placeholder='Send a message' />
         <input type="file" id ='image' accept='image/png, image/jpeg' hidden/>
