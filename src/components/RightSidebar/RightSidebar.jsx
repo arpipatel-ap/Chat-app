9d27d2c490994b3f2c7e4914e3cc6cdd1b31ba2f
arpipatel-ap <arpipatel96@gmail.com>
2024-08-27 14:19:35 -0400

    add rightside bar

--- src/components/RightSidebar/RightSidebar.jsx
+++ src/components/RightSidebar/RightSidebar.jsx
@@ -1,9 +1,15 @@
 import React from 'react';
 import "./RightSidebar.css";
+import assets from '../../assets/assets';
 
 const RightSidebar = () => {
   return (
-    <div></div>
+    <div className='rs'>
+      <div className="rs-profile">
+        <img src={assets.profile_img} alt="" />
+        <h3>Richard Sanford</h3>
+      </div>
+    </div>
   )
 }
 
