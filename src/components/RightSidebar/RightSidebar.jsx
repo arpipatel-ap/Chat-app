bb6b739ec3848c6f02a79e132fa07d246081d900
arpipatel-ap <arpipatel96@gmail.com>
2024-08-31 12:38:49 -0400

    add images in rightside bar

--- src/components/RightSidebar/RightSidebar.jsx
+++ src/components/RightSidebar/RightSidebar.jsx
@@ -7,9 +7,21 @@ const RightSidebar = () => {
     <div className='rs'>
       <div className="rs-profile">
         <img src={assets.profile_img} alt="" />
-        <h3>Richard Sanford</h3>
+        <h3>Richard Sanford <img src={assets.green_dot} className='dot' alt="" /></h3>
         <p>Hi, there I am Richard Sanford using chat app
         </p>
+      </div>
+      <hr />
+      <div className="rs-media">
+        <p>Media</p>
+        <div>
+          <img src={assets.pic1} alt="" />
+          <img src={assets.pic2} alt="" />
+          <img src={assets.pic3} alt="" />
+          <img src={assets.pic4} alt="" />
+          <img src={assets.pic1} alt="" />
+          <img src={assets.pic2} alt="" />
+        </div>
       </div>
     </div>
   )
