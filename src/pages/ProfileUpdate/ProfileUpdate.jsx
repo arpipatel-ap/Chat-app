41cc78adf21e7785e32d03c8ae3444611968daab
arpipatel-ap <arpipatel96@gmail.com>
2024-09-16 22:13:09 -0400

    add profileupdate page information

--- src/pages/ProfileUpdate/ProfileUpdate.jsx
+++ src/pages/ProfileUpdate/ProfileUpdate.jsx
@@ -1,10 +1,24 @@
 import React from 'react';
 import './ProfileUpdate.css';
+import assets from '../../assets/assets';
 
 const ProfileUpdate = () => {
   return (
-    <div>
-      
+    <div className='profile'>
+      <div className="profile-container">
+        <form>
+          <h3>Profile Details</h3>
+          <label htmlFor="avatar">
+            <input type="file" id="avatar" accept='.png, .jpg, .jpeg' hidden/>
+            <img src={assets.avatar_icon} alt="" />
+            upload profile image
+          </label>
+          <input type ="text" placeholder='Your name' required/>
+          <textarea placeholder='write profile bio' required/>
+          <button type ="submit">Save</button>
+        </form>
+        <img src={assets.logo_icon} alt="" />
+        </div>      
     </div>
   )
 }
