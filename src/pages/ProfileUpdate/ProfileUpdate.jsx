cb0a1d6ec39fbe27984be8467f8282d4d8f096c0
arpipatel-ap <arpipatel96@gmail.com>
2024-09-19 11:24:11 -0400

    add profile page css

--- src/pages/ProfileUpdate/ProfileUpdate.jsx
+++ src/pages/ProfileUpdate/ProfileUpdate.jsx
@@ -17,7 +17,7 @@ const ProfileUpdate = () => {
           <textarea placeholder='write profile bio' required/>
           <button type ="submit">Save</button>
         </form>
-        <img src={assets.logo_icon} alt="" />
+        <img className='profile-pic' src={assets.logo_icon} alt="" />
         </div>      
     </div>
   )
